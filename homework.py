01ea40e9f733bee51be7efe21e086b91b6ce1761
Александр Игнатенко <ignatenko1123@gmail.com>
2022-11-27 20:42:00 +0300

    fix v1

--- homework.py
+++ homework.py
@@ -1,38 +1,32 @@
+from dataclasses import dataclass, asdict
+from typing import TypedDict, Dict, Union, ClassVar
+
+MESSAGE = ('Тип тренировки: {}; '
+           'Длительность: {:.3f} ч.; '
+           'Дистанция: {:.3f} км; '
+           'Ср. скорость: {:.3f} км/ч; '
+           'Потрачено ккал: {:.3f}.')
+
+
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-
-    def __init__(self, training_type, duration, distance, speed,
-                 calories) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
-        return 'Тип тренировки: %s; ' \
-               'Длительность: %.3f ч.; ' \
-               'Дистанция: %.3f км; ' \
-               'Ср. скорость: %.3f км/ч; ' \
-               'Потрачено ккал: %.3f.' % (
-                   self.training_type, self.duration, self.distance,
-                   self.speed, self.calories)
-
-        pass
+        return MESSAGE.format(*asdict(self).values())
 
 
 class Training:
     """Базовый класс тренировки."""
 
-    LEN_STEP = 0.65
-    M_IN_KM = 1000
-    MIN_IN_H = 60
-    # минут в часе
-    S_IN_H = 3600
-
-    # секунд в часе
-
-    # рост в метрах
+    LEN_STEP: float = 0.65
+    M_IN_KM: int = 1000
+    MIN_IN_H: int = 60
 
     def __init__(self,
                  action: int,
@@ -43,22 +37,18 @@ class Training:
         self.weight = weight
         self.action = action
 
-        pass
-
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        distance = self.action * self.LEN_STEP / self.M_IN_KM
-        return distance
-        pass
+        return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
         mean_speed = self.get_distance() / self.duration
         return mean_speed
-        pass
 
-    def get_spent_calories(self) -> float:
+    def get_spent_calories(self):
         """Получить количество затраченных калорий."""
+        raise NotImplementedError("Please Implement this method")
         pass
 
     def show_training_info(self) -> InfoMessage:
@@ -66,16 +56,12 @@ class Training:
         return InfoMessage(self.__class__.__name__, self.duration,
                            self.get_distance(), self.get_mean_speed(),
                            self.get_spent_calories())
-        pass
 
 
 class Running(Training):
     """Тренировка: бег."""
-    CALORIES_MEAN_SPEED_MULTIPLIER = 18
-    CALORIES_MEAN_SPEED_SHIFT = 1.79
-
-    def __init__(self, action: int, duration: float, weight: float) -> None:
-        super().__init__(action, duration, weight)
+    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
+    CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
     def get_spent_calories(self) -> float:
         cal = self.CALORIES_MEAN_SPEED_MULTIPLIER
@@ -87,15 +73,13 @@ class Running(Training):
                           / self.M_IN_KM * duration)
         return spent_calories
 
-    pass
-
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    CALORIES_WEIGHT_MULTIPLIER = 0.035
-    CALORIES_SPEED_HEIGHT_MULTIPLIER = 0.029
-    KMH_IN_MSEC = 0.278
-    CM_IN_M = 100
+    CALORIES_WEIGHT_MULTIPLIER: float = 0.035
+    CALORIES_SPEED_HEIGHT_MULTIPLIER: float = 0.029
+    KMH_IN_MSEC: float = 0.278
+    CM_IN_M: int = 100
 
     def __init__(self, action: int, duration: float, weight: float,
                  height: float) -> None:
@@ -109,19 +93,17 @@ class SportsWalking(Training):
         c_wght = self.CALORIES_WEIGHT_MULTIPLIER
         c_sp = self.CALORIES_SPEED_HEIGHT_MULTIPLIER
         spent_calories = (c_wght * self.weight + (
-            (speed ** 2 / height) * c_sp
-            * self.weight)) * duration
+                (speed ** 2 / height) * c_sp
+                * self.weight)) * duration
         return spent_calories
-
-    pass
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
 
-    LEN_STEP = 1.38
-    CALORIES_MEAN_SPEED_SHIFT = 1.1
-    CALORIES_WEIGHT_MULTIPLIER = 2
+    LEN_STEP: float = 1.38
+    CALORIES_MEAN_SPEED_SHIFT: float = 1.1
+    CALORIES_WEIGHT_MULTIPLIER: int = 2
 
     def __init__(self, action: int, duration: float, weight: float,
                  length_pool: float, count_pool: int) -> None:
@@ -131,7 +113,7 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         mean_speed = self.length_pool * self.count_pool \
-            / self.M_IN_KM / self.duration
+                     / self.M_IN_KM / self.duration
         """Получить среднюю скорость движения."""
         return mean_speed
 
@@ -139,28 +121,21 @@ class Swimming(Training):
         temp = self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT
         # (средняя_скорость + 1.1) * 2 * вес * время_тренировки
         spent_calories = temp * self.CALORIES_WEIGHT_MULTIPLIER \
-            * self.weight * self.duration
+                         * self.weight * self.duration
         return spent_calories
-
-    pass
 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
 
-    if workout_type == 'SWM':
-        return Swimming(data[0], data[1], data[2], data[3], data[4])
-    elif workout_type == 'RUN':
-        return Running(data[0], data[1], data[2])
-    elif workout_type == 'WLK':
-        return SportsWalking(data[0], data[1], data[2], data[3])
-    pass
+    types: Dict[str, ClassVar] = {'SWM': Swimming, 'RUN': Running,
+                                  'WLK': SportsWalking}
+    return types[workout_type](*data)
 
 
 def main(training: Training) -> None:
     """Главная функция."""
     print(training.show_training_info().get_message())
-    pass
 
 
 if __name__ == '__main__':
